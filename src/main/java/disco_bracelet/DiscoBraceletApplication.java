d975bd66c3c3e999c86f3abad4b2468611aadc4a
MiladinKi <miladinkovacevic12@gmail.com>
2024-06-24 20:04:56 +0200

    Povezivanje repozitorijuma

---
+++ src/main/java/disco_bracelet/DiscoBraceletApplication.java
@@ -0,0 +1,13 @@
+package disco_bracelet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoBraceletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoBraceletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/disco_bracelet/DiscoBraceletApplicationTests.java
@@ -0,0 +1,13 @@
+package disco_bracelet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoBraceletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
